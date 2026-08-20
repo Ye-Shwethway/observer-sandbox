740d1f5fa5faed034b2e7f4e931cd83eaa346939
Mr. Thorne <85703541+Ye-Shwethway@users.noreply.github.com>
2026-08-20 19:21:10 +0630

    Install Sandbox batch delete world-layer extension

--- src/observer_sandbox/telegram_world_layers.py
+++ src/observer_sandbox/telegram_world_layers.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
 from . import telegram_world_layers_base as _base
+from .telegram_sandbox_batch_delete import install_sandbox_batch_delete_world_layers_extension
 from .telegram_world_layers_item_extension import install_item_world_layers_extension
 from .telegram_world_layers_item_edit_extension import install_item_edit_world_layers_extension
 
 install_item_world_layers_extension(_base)
 install_item_edit_world_layers_extension(_base)
+install_sandbox_batch_delete_world_layers_extension(_base)
 
 for _name in _base.__all__:
     globals()[_name] = getattr(_base, _name)
